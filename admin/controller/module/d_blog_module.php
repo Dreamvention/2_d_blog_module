e016356b38045054bc5a3ff4ae84e34e70380120
dmitriyzhuk <dmitriyzhuk@gmail.com>
2017-06-12 23:56:24 +0300

    fix

--- admin/controller/module/d_blog_module.php
+++ admin/controller/module/d_blog_module.php
@@ -61,6 +61,8 @@ class ControllerModuleDBlogModule extends Controller {
         $this->load->model('setting/setting');
         $this->load->model('d_shopunity/setting');
 
+        $this->model_module_d_blog_module->updateTables();
+
         //save post
         if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
 
@@ -405,6 +407,7 @@ class ControllerModuleDBlogModule extends Controller {
     public function install() {
         $this->load->model('module/d_blog_module');
         $this->model_module_d_blog_module->createTables( );
+        $this->model_module_d_blog_module->updateTables();
            
         if($this->d_shopunity){
 
