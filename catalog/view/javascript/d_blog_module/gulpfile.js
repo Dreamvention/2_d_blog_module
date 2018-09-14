7a5626fcda4bf9f09de0b951221dc35bc86e2ec8
pavel <php5.drvn@gmail.com>
2018-09-14 13:37:57 +0300

    replace gulp destination folder; fix stars efect

--- catalog/view/javascript/d_blog_module/gulpfile.js
+++ catalog/view/javascript/d_blog_module/gulpfile.js
@@ -6,12 +6,25 @@ var gulp = require('gulp'),
 	cleanCSS = require('gulp-clean-css'),
 	sourcemaps = require('gulp-sourcemaps'),
 	autoprefixer = require('gulp-autoprefixer');
+var path = require('path');
+var fs = require('fs');
+var baseDir = path.resolve(__dirname, "../../../");
+var themeDir = path.join(baseDir, 'view/theme/default');
+var sassDir = path.join(themeDir, 'stylesheet/d_blog_module');
+var skinDir = path.join(sassDir, 'theme');
 
+function getFolders(dir) {
+	return fs.readdirSync(dir)
+		.filter(function (file) {
+			return fs.statSync(path.join(dir, file)).isDirectory();
+		});
+}
 gulp.task('browser-sync', function () {
 	browserSync({
-		proxy: 'http://localhost/302/d_blog_module/'
+		proxy: 'http://localhost/302/2_d_blog_module/'
 	});
 });
+
 gulp.task('sass-bm-bootsrap', function () {
 	return gulp.src('catalog/view/theme/default/stylesheet/d_blog_module/bootstrap.scss',{ base: 'catalog/view/theme/default/stylesheet/d_blog_module' })
 		.pipe(sourcemaps.init())
@@ -21,7 +34,19 @@ gulp.task('sass-bm-bootsrap', function () {
 		.pipe(gulp.dest('./catalog/view/theme/default/stylesheet/d_blog_module'))
 		.pipe(browserSync.stream({match: '**/*.css'}));
 });
-
+gulp.task('sass_multi', function () {
+	var folders = getFolders(skinDir);
+	var tasks = folders.map(function (folder) {
+		return gulp.src(path.join(skinDir, folder, folder + '.s*ss'))
+			.pipe(sourcemaps.init())
+			.pipe(sass().on('error', sass.logError))
+			.pipe(autoprefixer(['last 15 versions']))
+			.pipe(sourcemaps.write('./'))
+			.pipe(gulp.dest(path.join(sassDir, 'theme')))
+			.pipe(browserSync.stream({match: '**/*.css'}));
+	});
+	return tasks;
+});
 gulp.task('sass', function () {
 	return gulp.src('catalog/view/theme/default/stylesheet/d_blog_module/d_blog_module.scss')
 		.pipe(sourcemaps.init())
